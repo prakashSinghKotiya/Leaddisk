af3aed3088a92d6f26fdbb502a00e0aa2b6662ee
Prakash Singh Kotiya <prakashkotiya100@gmail.com>
2026-07-26 01:31:52 +0530

    updated database

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://leaddisk-production.up.railway.app/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
